057334b8c07e450c8991a4a4fdf363cfcecfd00d
dimelorobert <airbusjayrobert@gmail.com>
2020-06-10 02:08:53 +0200

    poniendo colorinchos a los botones y barra de menu

--- proyecto-notas/src/App.vue
+++ proyecto-notas/src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Raleway', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -18,15 +19,35 @@
 }
 
 #nav {
+  display: flex;
+  justify-content: space-around;
   padding: 30px;
+  background-color: #21d4fd;
+  background-image: linear-gradient(19deg, #21d4fd 0%, #b721ff 100%);
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: white;
+  text-decoration: none;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: white;
+  text-decoration: none;
+}
+button {
+  border: none;
+  background-color: #21d4fd;
+  background-image: linear-gradient(19deg, #21d4fd 0%, #b721ff 100%);
+  border-radius: 0.3rem;
+  height: 2rem;
+  color: white;
+  font-family: 'Raleway', sans-serif;
+  font-weight: 700;
+  outline: none;
+}
+.notas {
+  margin: 1rem 0;
 }
 </style>
